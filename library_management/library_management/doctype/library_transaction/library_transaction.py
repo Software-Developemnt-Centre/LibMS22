3af574e5f35500e9e63f949b872c0d46810488bf
H.S.Rai <hardeep.rai@gmail.com>
2022-10-12 12:41:01 +0530

    Article chmaged to Item, Copy effect

--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -18,30 +18,30 @@ class LibraryTransaction(Document):
     def before_submit(self):
         if self.type == "Issue":
             self.validate_issue()
-            # set the article status to be Issued
-            article = frappe.get_doc("Article", self.article)
-            article.status = "Issued"
-            article.save()
+            # set the item status to be Issued
+            item = frappe.get_doc("Item", self.item)
+            item.status = "Issued"
+            item.save()
 
         elif self.type == "Return":
             self.validate_return()
-            # set the article status to be Available
-            article = frappe.get_doc("Article", self.article)
-            article.status = "Available"
-            article.save()
+            # set the item status to be Available
+            item = frappe.get_doc("Item", self.item)
+            item.status = "Available"
+            item.save()
 
     def validate_issue(self):
         self.validate_membership()
-        article = frappe.get_doc("Article", self.article)
-        # article cannot be issued if it is already issued
-        if article.status == "Issued":
-            frappe.throw("Article is already issued by another member")
+        item = frappe.get_doc("Item", self.item)
+        # item cannot be issued if it is already issued
+        if item.status == "Issued":
+            frappe.throw("Item is already issued by another member")
 
     def validate_return(self):
-        article = frappe.get_doc("Article", self.article)
-        # article cannot be returned if it is not issued first
-        if article.status == "Available":
-            frappe.throw("Article cannot be returned without being issued first")
+        item = frappe.get_doc("Item", self.item)
+        # item cannot be returned if it is not issued first
+        if item.status == "Available":
+            frappe.throw("Item cannot be returned without being issued first")
 
     def validate_membership(self):
         # check if a valid membership exist for this library member
